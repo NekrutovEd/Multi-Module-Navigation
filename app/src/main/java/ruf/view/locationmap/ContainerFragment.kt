09dd77b9495e35a3c260da727022bb5ef5e1891a
Edvard <edvard94@mail.ru>
2020-02-14 10:08:49 +0300

    ContainerFragment

---
+++ app/src/main/java/ruf/view/locationmap/ContainerFragment.kt
@@ -0,0 +1,39 @@
+package ruf.view.locationmap
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ruf.view.locationmap.navigator.Navigator
+import ruf.view.locationmap.navigator.NavigatorModule
+import ruf.view.locationmap.navigator.NavigatorModule.Companion.injectNavigator
+import ruf.view.locationmap.navigator.list.ListModule
+import toothpick.ktp.delegate.inject
+
+class ContainerFragment : Fragment() {
+
+    private val mainNavigator: Navigator by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        NavigatorModule(R.id.container, ContainerFragment::class).installModule()
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.container_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        injectNavigator(ContainerFragment::class)
+        mainNavigator.forwardIfEmpty(ListModule())
+        mainNavigator.attachFragmentManager(childFragmentManager)
+    }
+
+    override fun onDestroyView() {
+        mainNavigator.detachFragmentManager()
+        super.onDestroyView()
+    }
+}
--- app/src/main/java/ruf/view/locationmap/MainActivity.kt
+++ app/src/main/java/ruf/view/locationmap/MainActivity.kt
@@ -2,29 +2,17 @@ package ruf.view.locationmap
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import ruf.view.locationmap.navigator.Navigator
-import ruf.view.locationmap.navigator.NavigatorModule
-import ruf.view.locationmap.navigator.NavigatorModule.Companion.injectNavigator
-import ruf.view.locationmap.navigator.list.ListModule
-import toothpick.ktp.delegate.inject
 
 
 class MainActivity : AppCompatActivity() {
-
-    private val mainNavigator: Navigator by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity)
 
-        NavigatorModule(R.id.container, application).installModule()
-        injectNavigator(application)
-        mainNavigator.forwardIfEmpty(ListModule())
-        mainNavigator.attachFragmentManager(supportFragmentManager)
-    }
-
-    override fun onDestroy() {
-        mainNavigator.detachFragmentManager()
-        super.onDestroy()
+        supportFragmentManager.findFragmentById(R.id.container)
+            ?: supportFragmentManager.beginTransaction()
+                .add(R.id.container, ContainerFragment())
+                .commit()
     }
 }
